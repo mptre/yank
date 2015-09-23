mptre/yank

commit 58b4274e3a48b9b0214c7005bd30d40311c9eb14

Cleanup
Fixed violations according to `style(9)' in yank.1:

  - Join options without operands in synopsis

  - Sort options alphabetically disregarding whether the option takes an
    argument or not

yank.c:

  - Update comment

  - Move memory allocation in order to better estimate of the number of
    remaining arguments

  - Join options without operands in usage output

  - Remove redundant static keyword

  - Fix indent

  - Remove reset variable from `tmain' since `field' only writes to the
    given pointers once a valid field is found.

yank.c
@@ -9,7 +9,7 @@
 #include <termios.h>
 #include <unistd.h>
 
-/* Control characters */
+/* Terminal capabilities */
 #define T_CLR_EOS             "\033[J"
 #define T_COLUMN_ADDRESS      "\033[%dG"
 #define T_CURSOR_INVISIBLE    "\033[?25l"
@@ -65,14 +65,6 @@ args(int argc, const char **argv)
 	size_t n;
 	int c, i;
 
-	/* Ensure space for null terminator. */
-	n = (argc + 1)*sizeof(const char *);
-	yankargv = malloc(n);
-	if (!yankargv)
-		perror("malloc");
-	memset(yankargv, 0, n);
-	yankargv[0] = YANKCMD;
-
 	while ((c = getopt(argc, (char * const *) argv, "lvd:")) != -1) {
 		switch (c) {
 		case 'd':
@@ -87,8 +79,7 @@ args(int argc, const char **argv)
 		default:
 		usage:
 			fputs("usage: yank "
-			      "[-l] "
-			      "[-v] "
+			      "[-l | -v] "
 			      "[-d delim] "
 			      "[-- command [argument ...]]\n", stderr);
 			exit(1);
@@ -97,6 +88,13 @@ args(int argc, const char **argv)
 	if (optind < argc && strncmp(argv[optind - 1] , "--", 3))
 		goto usage;
 
+	/* Ensure space for yank command and null terminator. */
+	n = (argc - optind + 2)*sizeof(const char *);
+	yankargv = malloc(n);
+	if (!yankargv)
+		perror("malloc");
+	memset(yankargv, 0, n);
+	yankargv[0] = YANKCMD;
 	for (i = optind; i < argc; i++)
 		yankargv[i - optind] = argv[i];
 }
@@ -105,7 +103,7 @@ args(int argc, const char **argv)
  * Returns the next unicode rune offset relative to s + offset in the direction
  * given by inc.
  */
-static ssize_t
+ssize_t
 rune(const char *s, size_t offset, int inc)
 {
 	ssize_t i;
@@ -220,7 +218,7 @@ yank(void)
 	if (write(fd[1], sel.v, sel.nmemb) < 0)
 		perror("write");
 	if (close(fd[1]) < 0)
-	    perror("close");
+		perror("close");
 	pid = fork();
 	switch (pid) {
 	case -1:
@@ -341,53 +339,50 @@ tend(void)
 void
 tmain(void)
 {
-	struct { size_t start; size_t stop; } f, reset;
+	size_t start, stop;
 	int c;
 
-	f.start = f.stop = 0;
-	if (field(in.v, 1, &f.start, &f.stop))
-		tdraw(in.v, in.pmemb, f.start, f.stop);
+	start = stop = 0;
+	if (field(in.v, 1, &start, &stop))
+		tdraw(in.v, in.pmemb, start, stop);
 	else
 		twrite(in.v, in.pmemb);
 	for (;;) {
-		reset = f;
 		if (read(tty.in, &c, 1) < 0)
 			perror("read");
 		switch (c & 0xFF) {
 		case '\n':
-			sel.nmemb = f.stop - f.start + 1;
-			sel.v = in.v + f.start;
+			sel.nmemb = stop - start + 1;
+			sel.v = in.v + start;
 			/* FALLTHROUGH */
 		case CONTROL('C'):
 		case CONTROL('D'):
 			return;
 		case CONTROL('A'):
-			f.start = 0;
+			start = 0;
 		if (0) {
 		case CONTROL('N'):
-			f.start = f.stop + rune(in.v, f.stop, 1);
+			start = stop + rune(in.v, stop, 1);
 		}
-			if (field(in.v, 1, &f.start, &f.stop))
-				break;
-			f = reset;
-			continue;
+			if (!field(in.v, 1, &start, &stop))
+				continue;
+			break;
 		case CONTROL('E'):
-			f.stop = in.pmemb - 1;
+			stop = in.pmemb - 1;
 		if (0) {
 		case CONTROL('P'):
-			f.stop = f.start + rune(in.v, f.start, -1);
+			stop = start + rune(in.v, start, -1);
 		}
-			if (field(in.v, -1, &f.stop, &f.start))
-				break;
-			f = reset;
-			continue;
+			if (!field(in.v, -1, &stop, &start))
+				continue;
+			break;
 		default:
 			continue;
 		}
 		if (in.nlines)
 			tprintf(T_CURSOR_UP, in.nlines);
 		tprintf(T_COLUMN_ADDRESS, 1);
-		tdraw(in.v, in.pmemb, f.start, f.stop);
+		tdraw(in.v, in.pmemb, start, stop);
 	}
 }
 

yank.1
@@ -6,8 +6,7 @@
 .Nd yank terminal output to clipboard
 .Sh SYNOPSIS
 .Nm yank
-.Op Fl l
-.Op Fl v
+.Op Fl l | v
 .Op Fl d Ar delim
 .Op Fl - Ar command Op Ar argument ...
 .Sh DESCRIPTION
@@ -40,14 +39,14 @@ option will be used as the yank command, see
 .Em EXAMPLES .
 .Sh OPTIONS
 .Bl -tag -width -indent
-.It Fl l
-use only new line and tab characters as field delimiters
-.It Fl v
-prints version
 .It Fl d Ar delim
 use all characters in
 .Ar delim
 as field delimiters
+.It Fl l
+use only new line and tab characters as field delimiters
+.It Fl v
+prints version
 .It Fl - Ar command Op Ar argument ...
 use
 .Ar command

commit 29e1b78bd07ef1230143a01837f6d8dfcb38ccb0

Fix bounds navigation bug
This is a regression from 58b4274 after removing the local reset in
`tmain'. Steps to reproduce:

  1. ls | yank

  2. Move the selection to the end by pressing `Ctrl-E'.

  3. Move the selection to the right by pressing `Ctrl-N'. The selection
     is not moved since there is no more data.

  3. Move the selection to the left by pressing `Ctrl-P'. The selection
     is not moved since the `start' variable points beyond the input
     data.

This bug also occurs in the opposite direction.

yank.c
@@ -355,7 +355,7 @@ tend(void)
 void
 tmain(void)
 {
-	size_t start, stop;
+	size_t start, stop, t;
 	int c;
 
 	start = stop = 0;
@@ -375,22 +375,24 @@ tmain(void)
 		case CONTROL('D'):
 			return;
 		case CONTROL('A'):
-			start = 0;
+			t = 0;
 		if (0) {
 		case CONTROL('N'):
-			start = stop + rune(in.v, stop, 1);
+			t = stop + rune(in.v, stop, 1);
 		}
-			if (!field(in.v, 1, &start, &stop))
+			if (!field(in.v, 1, &t, &stop))
 				continue;
+			start = t;
 			break;
 		case CONTROL('E'):
-			stop = in.pmemb - 1;
+			t = in.pmemb - 1;
 		if (0) {
 		case CONTROL('P'):
-			stop = start + rune(in.v, start, -1);
+			t = start + rune(in.v, start, -1);
 		}
-			if (!field(in.v, -1, &stop, &start))
+			if (!field(in.v, -1, &t, &start))
 				continue;
+			stop = t;
 			break;
 		default:
 			continue;