mptre/yank

commit d704b6ac79f950578ac4323b30a539ce31cc0931

Add MANPREFIX support to install task

Makefile
@@ -3,6 +3,7 @@ VERSION=	0.6.4
 YANKCMD=	xsel
 
 PREFIX=		/usr/local
+MANPREFIX=	${PREFIX}/share/man
 
 CFLAGS+=	-Os -pedantic -std=c99 -Wall -Werror -Wextra
 CPPFLAGS+=	-DVERSION=\"${VERSION}\" -DYANKCMD=\"${YANKCMD}\"
@@ -18,9 +19,9 @@ install: yank
 	@mkdir -p "${PREFIX}/bin"
 	@cp -f yank "${PREFIX}/bin"
 	@chmod 755 "${PREFIX}/bin/yank"
-	@echo "yank.1 -> ${PREFIX}/share/man/man1/yank.1"
-	@mkdir -p "${PREFIX}/share/man/man1"
-	@cp -f yank.1 "${PREFIX}/share/man/man1/yank.1"
+	@echo "yank.1 -> ${MANPREFIX}/man1/yank.1"
+	@mkdir -p "${MANPREFIX}/man1"
+	@cp -f yank.1 "${MANPREFIX}/man1/yank.1"
 	@chmod 644 "${PREFIX}/share/man/man1/yank.1"
 
 .PHONY: clean install

commit 612889723e0e413e9d4f90a7ef7ca7accb1ec839

Fix typo in Makefile
Regression from d704b6a.

Makefile
@@ -22,7 +22,7 @@ install: yank
 	@echo "yank.1 -> ${MANPREFIX}/man1/yank.1"
 	@mkdir -p "${MANPREFIX}/man1"
 	@cp -f yank.1 "${MANPREFIX}/man1/yank.1"
-	@chmod 644 "${PREFIX}/share/man/man1/yank.1"
+	@chmod 644 "${MANPREFIX}/man1/yank.1"
 
 .PHONY: clean install
 