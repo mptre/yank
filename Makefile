mptre/yank

commit 1c43c464f38688930be71ac4e21630c0360cd3ce

Control output binary name with PROG variable in the Makefile
Issue #35

Makefile
@@ -5,24 +5,26 @@ YANKCMD=	xsel
 PREFIX=		/usr/local
 MANPREFIX=	${PREFIX}/share/man
 
+PROG=		yank
+
 INSTALL_PROGRAM=	install -s -m 0755
 INSTALL_MAN=		install -m 0644
 
 CFLAGS+=	-pedantic -Wall -Werror -Wextra
 CPPFLAGS+=	-DVERSION=\"${VERSION}\" -DYANKCMD=\"${YANKCMD}\"
 
-all: yank
+all: ${PROG}
 
-yank: yank.c
-	${CC} ${CFLAGS} -o $@ $< ${LDFLAGS} ${CPPFLAGS}
+${PROG}: yank.c
+	${CC} ${CFLAGS} -o ${PROG} yank.c ${LDFLAGS} ${CPPFLAGS}
 
 clean:
-	rm yank
+	rm ${PROG}
 
-install: yank
+install: ${PROG}
 	@mkdir -p ${DESTDIR}${PREFIX}/bin
 	@mkdir -p ${DESTDIR}${MANPREFIX}/man1
-	${INSTALL_PROGRAM} yank ${DESTDIR}${PREFIX}/bin
+	${INSTALL_PROGRAM} ${PROG} ${DESTDIR}${PREFIX}/bin
 	${INSTALL_MAN} yank.1 ${DESTDIR}${MANPREFIX}/man1
 
 .PHONY: all clean install

commit d70bcf94615ad0fb43cff77a61b18ae2d4fe030c

Fix indent in Makefile
Regression from commit 1c43c46 (Control output binary name with PROG
variable in the Makefile).

Makefile
@@ -5,7 +5,7 @@ YANKCMD=	xsel
 PREFIX=		/usr/local
 MANPREFIX=	${PREFIX}/share/man
 
-PROG=		yank
+PROG=	yank
 
 INSTALL_PROGRAM=	install -s -m 0755
 INSTALL_MAN=		install -m 0644