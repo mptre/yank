9ecb1f5676941482f7ea2a1e8846a7adb0f58be4
Anton Lindqvist <anton.lindqvist@gmail.com>
2017-04-23 11:31:30 +0200

    snprintf(3) can return -1 on failure

--- yank.c
+++ yank.c
@@ -100,11 +100,13 @@ strtopat(const char *s)
 	const char	*fmt = "[^%s\f\n\r\t]+";
 	char		*pat;
 	size_t		len;
+	int		n;
 
 	len = strlen(s) + strlen(fmt) + 1;
 	if ((pat = malloc(len)) == NULL)
 		err(1, NULL);
-	if (snprintf(pat, len, fmt, s) >= (ssize_t)len)
+	n = snprintf(pat, len, fmt, s);
+	if (n == -1 || n >= (ssize_t)len)
 		errx(1, "pattern too long");
 
 	return pat;
